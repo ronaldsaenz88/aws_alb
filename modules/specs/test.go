af9733cfa5cbabd0ee529b2d22da029b6f8cb32c
RSAENZ <ronaldsaenz88@gmail.com>
2021-03-17 18:56:42 -0500

    AWS - ALB - #4

--- modules/specs/test.go
+++ modules/specs/test.go
@@ -1,7 +1,10 @@
 package test
 
 import (
+	"fmt"
 	"testing"
+	"time"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
@@ -17,4 +20,18 @@ func TestGCPModule(t *testing.T) {
 	}()
 	terraform.Init(t, terraformOptions)
 	terraform.Apply(t, terraformOptions)
+
+	publicIp := terraform.Output(t, terraformOptions, "aws_web_master_ip")
+
+	url := fmt.Sprintf("http://%s", publicIp)
+
+	maxRetries := 30
+	timeBetweenRetries := 5 * time.Second
+
+	http_helper.HttpGetWithRetryWithCustomValidation(t, url, maxRetries, timeBetweenRetries, validate)
+	//http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
+}
+
+func validate(status int, _ string) bool {
+	return status == 200
 }
